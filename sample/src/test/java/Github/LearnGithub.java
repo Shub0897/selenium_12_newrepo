b3fa9821ffe663062e77430f1ea0719ef1425e47
Bhoomka <bhoomikagautam08@gmail.com>
2025-02-01 10:57:15 +0530

    Github Added

---
+++ sample/src/test/java/Github/LearnGithub.java
@@ -0,0 +1,12 @@
+package Github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LearnGithub {
+	@Test
+	public void learn() {
+		Reporter.log("Github class Added",true);
+	}
+
+}
