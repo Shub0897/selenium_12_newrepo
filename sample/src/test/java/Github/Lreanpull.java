8069802ce83b3699f985b320d827947816d5b677
Bhoomka <bhoomikagautam08@gmail.com>
2025-02-01 11:52:52 +0530

    To learn pull

---
+++ sample/src/test/java/Github/Lreanpull.java
@@ -0,0 +1,14 @@
+package Github;
+
+	import org.testng.Reporter;
+	import org.testng.annotations.Test;
+
+	public class Lreanpull {
+		@Test
+		public void learn() {
+			Reporter.log("Github class Added",true);
+		}
+
+	}
+
+
